81abb8b86ef9eaa488041992e4cbb6a946da69ba
ludics <me@ludics.cn>
2021-05-27 07:42:57 +0800

    update lr scheduler

--- main_adsh.py
+++ main_adsh.py
@@ -161,7 +161,7 @@ def main(args, config):
         flops = model_without_ddp.flops()
         logger.info(f"number of GFLOPs: {flops / 1e9}")
 
-    lr_scheduler = build_scheduler(config, optimizer, len(retrieval_dataloader))
+    lr_scheduler = build_scheduler(config, optimizer, config.HASH.NUM_SAMPLES)
 
     if config.AUG.MIXUP > 0.:
         # smoothing is handled with mixup label transform
