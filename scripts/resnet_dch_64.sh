0fb47eb91c5adf774c28bd05167e48710c351c0f
ludics <me@ludics.cn>
2021-05-29 11:02:18 +0000

    add gpu checker

--- scripts/resnet_dch_64.sh
+++ scripts/resnet_dch_64.sh
@@ -2,7 +2,7 @@ echo "CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_no
     --cfg configs/resnet152.yaml  --data-path /data/fine-grained/cub-200-2011/CUB_200_2011/ \
     --batch-size 64 --accumulation-steps 2  --hash_bit 48 --tag hash_48_resnet152_clsm1 \
     --valid_rank 0 --amp-opt-level O0"
-CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch --nproc_per_node 2 --master_port 12359  main_hash.py \
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node 2 --master_port 12359  main_hash.py \
     --cfg configs/resnet152.yaml  --data-path /data/fine-grained/cub-200-2011/CUB_200_2011/ \
-    --batch-size 64 --accumulation-steps 2  --hash_bit 48 --tag hash_48_resnet152_clsm1 \
+    --batch-size 64 --accumulation-steps 2  --hash_bit 64 --tag hash_64_resnet152_clsm1 \
     --valid_rank 0 --amp-opt-level O0
