f67368450ef9cf851c13782801a3e134c875ebb8
ludics <me@ludics.cn>
2021-05-29 20:39:05 +0800

    update scripts

--- scripts/resnet_dch.sh
+++ scripts/resnet_dch.sh
@@ -2,7 +2,7 @@ echo "CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_no
     --cfg configs/resnet152.yaml  --data-path /data/fine-grained/cub-200-2011/CUB_200_2011/ \
     --batch-size 64 --accumulation-steps 2  --hash_bit 32 --tag hash_32_resnet152_clsm1 \
     --valid_rank 0 --amp-opt-level O0"
-CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node 2 --master_port 12358  main_hash.py \
-    --cfg configs/resnet152.yaml  --data-path /data/fine-grained/cub-200-2011/CUB_200_2011/ \
-    --batch-size 64 --accumulation-steps 2  --hash_bit 32 --tag hash_32_resnet152_clsm1 \
+CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch --nproc_per_node 2 --master_port 12358  main_hash.py \
+    --cfg configs/resnet50.yaml  --data-path /data/ludi/datasets/cub-200-2011/CUB_200_2011/ \
+    --batch-size 64 --accumulation-steps 2  --hash_bit 16 --tag hash_16_resnet152_clsm1 \
     --valid_rank 0 --amp-opt-level O0
