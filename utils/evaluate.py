a563f295afbdb467e5cf2b75a1c0b7eb5377dc89
ludics <me@ludics.cn>
2021-09-03 14:23:40 +0800

    add cont map

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -52,3 +52,60 @@ def mean_average_precision(query_code,
 
     mean_AP = mean_AP / num_query
     return mean_AP
+
+
+def mean_average_precision_cont(query_feat,
+                           database_feat,
+                           query_labels,
+                           database_labels,
+                           topk=None,
+                           ):
+    """
+    Calculate mean average precision(map).
+
+    Args:
+        query_feat (torch.Tensor): Query data feat.
+        database_feat (torch.Tensor): Database data feat.
+        query_labels (torch.Tensor): Query data targets, one-hot
+        database_labels (torch.Tensor): Database data targets, one-host
+        device (torch.device): Using CPU or GPU.
+        topk (int): Calculate top k data map.
+
+    Returns:
+        meanAP (float): Mean Average Precision.
+    """
+    num_query = query_labels.shape[0]
+    mean_AP = 0.0
+
+    query_norm = query_feat.norm(p=2, dim=1)
+    database_norm = database_feat.norm(p=2, dim=1)
+
+    for i in tqdm(range(num_query)):
+        # Retrieve images from database
+        retrieval = (query_labels[i, :] @ database_labels.t() > 0).float()
+
+        # Calculate hamming distance
+        # hamming_dist = 0.5 * (database_code.shape[1] - query_code[i, :] @ database_code.t())
+
+        cos_dist = query_feat[i, :] @ database_feat.t() / query_norm[i] / database_norm
+
+        # Arrange position according to hamming distance
+        retrieval = retrieval[torch.argsort(cos_dist)][:topk]
+
+        # Retrieval count
+        retrieval_cnt = retrieval.sum().int().item()
+
+        # Can not retrieve images
+        if retrieval_cnt == 0:
+            continue
+
+        # Generate score for every position
+        score = torch.linspace(1, retrieval_cnt, retrieval_cnt).cuda()
+
+        # Acquire index
+        index = (torch.nonzero(retrieval == 1).squeeze() + 1.0).float()
+
+        mean_AP += (score / index).mean()
+
+    mean_AP = mean_AP / num_query
+    return mean_AP
